71e0082f8778e03063ef4c7301a4586e626317af
fhdbuaa <fhdbuaa@users.noreply.github.com>
2017-08-22 17:11:54 +0800

    del

--- hearth/edit_json.py
+++
@@ -1,9 +0,0 @@
-# _*_ coding: utf-8 _*_
-from __future__ import unicode_literals
-import json
-
-if __name__ == '__main__':
-    with open('src/deck.json') as f:
-        info = json.loads(f.read())
-    for i in info:
-        print i, info[i]
