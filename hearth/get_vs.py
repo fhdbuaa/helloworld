3cf96ec6ef4422ba87d605d40fd057bf6055fc08
fhdbuaa <fhdbuaa@users.noreply.github.com>
2017-10-10 18:03:27 +0800

    fresh vs code

--- hearth/get_vs.py
+++ hearth/get_vs.py
@@ -15,17 +15,23 @@ def d_format(word):
     return word.strip('=')
 
 
+def save_img(url, key, unique):
+    print url
+    img = requests.get(url, stream=True)
+    if img.status_code == 200:
+        open('vs/' + char[key] + '-' + unicode(unique) + '.png', str('wb')).write(img.content)
+    return char[key] + '-' + unicode(unique) + '.png'
+
+
+char = {'潜行者': 'Rogue', '牧师': 'Priest', '德鲁伊': 'Druid', '术士': 'Warlock', '猎人': 'Hunter', '法师': 'Mage',
+        '萨满': 'Shaman', '圣骑士': 'Paladin', '战士': 'Warrior', '天梯环境': 'Environment', '强度排序': 'Rank'}
 if __name__ == '__main__':
-    char = {'潜行者': 'Rogue', '牧师': 'Priest', '德鲁伊': 'Druid', '术士': 'Warlock', '猎人': 'Hunter', '法师': 'Mage',
-            '萨满': 'Shaman', '圣骑士': 'Paladin', '战士': 'Warrior', '天梯环境': 'Environment', '强度排序': 'Rank'}
-    exceptions_list = {}
-    pinyin = xpinyin.Pinyin()
     deck_info = {'Environment': {}, 'Rogue': [], 'Priest': [], }
     tid = 12582689
-    ts_url = 'http://bbs.nga.cn/read.php?tid=' + unicode(tid)
+    vs_url = 'http://bbs.nga.cn/read.php?tid=' + unicode(tid)
     web = requests.session()
     web.trust_env = False
-    data = web.get(ts_url)
+    data = web.get(vs_url)
     # print data.content
     soup = BeautifulSoup(data.content, "html.parser")
     title = ''
@@ -39,17 +45,11 @@ if __name__ == '__main__':
             i = 0
         print string, '====', title
         if title in char and '[h]' not in string:
-            if string_.endswith('.png'):
+            if string_.endswith('.png') and string_.startswith('./'):
                 imgUrl = r'http://img.ngacn.cc/attachments' + string_[string_.index('/'):]
-                img = requests.get(imgUrl, stream=True)
-                with open('vs/' + char[title] + '-' + unicode(i) + '.png', str('wb')) as png:
-                    for chunk in img.iter_content(chunk_size=1024):
-                        if chunk:  # filter out keep-alive new chunks
-                            png.write(chunk)
-                            png.flush()
-                    temp['png'] += [char[title] + '-' + unicode(i) + '.png']
-                    i += 1
-            else:
+                temp['png'] += ['http://longlang.oschina.io/'+save_img(imgUrl, title, i)]
+                i += 1
+            elif not string_.endswith('.png'):
                 temp['info'] += string_
             deck_info[char[title]] = temp
     print json.dumps(deck_info, ensure_ascii=False)
